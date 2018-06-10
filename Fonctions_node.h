2f32047eb77736dee37c7f530c805fbac5ecfe1f
PC-MAEL-1\maelr_000 <mael.rouxel@outlook.fr>
2018-06-10 13:18:05 +0200

    Inode V1.2
    Création de fonctions de test dans le main

--- Fonctions_node.h
+++ Fonctions_node.h
@@ -2,3 +2,4 @@
 void Initialiser_BLOC(BLOC block);
 void Initialiser_META(META *metadata);
 void Initialiser_INODE(INODE *node);
+void Initialiser_DISK(DISK *partition);
--- main.c
+++ main.c
@@ -5,14 +5,42 @@
 #include "fonctions_node.h"
 
 void test_creation_INODE();
+bool test_initialisation_DISK();
+
 
 int main()
 {
     test_creation_INODE();
+    printf(test_initialisation_DISK()? " + test_initialisation_DISK : OK" : " !! test_initialisation_DISK : KO");
+
 
     return 0;
 }
 
+bool test_initialisation_DISK()
+{
+    DISK p1;
+    bool err_init = true;
+
+    Initialiser_DISK(&p1);
+    if(p1.last_id != 1)
+    {
+        printf("ERR INIT DISK : LAST_ID != 1 (%d)\n",p1.last_id);
+        err_init=false;
+    }
+
+    for(int i=0;i<TAILLE_MAX_DISK;i++)
+    {
+        if(p1.superbloc[i]!=NULL)
+        {
+            printf("ERR INIT DISK : SUPERBLOCK NOT NULL (BLOCK %d)\n",i);
+            err_init = false;
+            break;
+        }
+    }
+    return err_init;
+
+}
 
 void test_creation_INODE()
 {

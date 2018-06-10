536c56d63003a676d853e2c83e2a3ceea2bbc535
PC-MAEL-1\maelr_000 <mael.rouxel@outlook.fr>
2018-06-10 13:54:29 +0200

    Inode v1.4
    Définitions des macros pour les messages d'erreur

--- main.c
+++ main.c
@@ -9,11 +9,15 @@
 bool test_initialisation_INODE();
 bool test_initialisation_DISK();
 
+#define TEST(test_fonc)  printf(test_fonc()? " + %s : OK\n" : " !! %s : KO\n",  #test_fonc );
+#define ERREUR(message, code) printf(message, code); \
+        test_ok = false;
 
 int main()
 {
-    printf(test_initialisation_INODE()? " + test_initialisation_INODE : OK\n" : " !! test_initialisation_INODE : KO\n");
-    printf(test_initialisation_DISK()? " + test_initialisation_DISK : OK\n" : " !! test_initialisation_DISK : KO\n");
+    TEST(test_initialisation_INODE)
+    TEST(test_initialisation_DISK);
+
 
 
     return 0;
@@ -27,16 +31,14 @@ bool test_initialisation_DISK()
     Initialiser_DISK(&p1);
     if(p1.last_id != 1)
     {
-        printf("ERR INIT DISK : LAST_ID != 1 (%d)\n",p1.last_id);
-        test_ok=false;
+        ERREUR("ERR INIT DISK : LAST_ID != 1 (%d)\n", p1.last_id)
     }
 
     for(int i=0;i<TAILLE_MAX_DISK;i++)
     {
         if(p1.superbloc[i]!=NULL)
         {
-            printf("ERR INIT DISK : SUPERBLOCK NOT NULL (BLOCK %d)\n",i);
-            test_ok = false;
+            ERREUR("ERR INIT DISK : SUPERBLOCK NOT NULL (BLOCK %d)\n",i)
             break;
         }
     }
@@ -51,23 +53,21 @@ bool test_initialisation_INODE()
     Initialiser_INODE(&i);
     if(i.id != -1)
     {
-        printf("ERR INIT INODE : ID INITIALIZATION WRONG (id = %d)\n",i.id);
-        test_ok = false;
+        ERREUR("ERR INIT INODE : ID INITIALIZATION WRONG (id = %d)\n",i.id)
     }
     if(i.nom != NULL)
     {
-        printf("ERR INIT INODE : NAME NOT NULL (%s)\n",i.nom);
-        test_ok = false;
+        ERREUR("ERR INIT INODE : NAME NOT NULL (%s)\n",i.nom)
     }
     if(i.metadata.taille_fichier != 0)
     {
-        printf("ERR INIT INODE : FILE SIZE NOT NULL (%d)\n",i.metadata.taille_fichier);
-        test_ok = false;
+        ERREUR("ERR INIT INODE : FILE SIZE NOT NULL (%d)\n",i.metadata.taille_fichier)
     }
     if(strcmp(i.metadata.date_creation, "00/00/0000") != 0)
     {
-        printf("ERR INIT INODE : CREATION DATE INITIALIZATION WRONG (%s)\n",i.metadata.date_creation);
-        test_ok = false;
+        ERREUR("ERR INIT INODE : CREATION DATE INITIALIZATION WRONG (%s)\n",i.metadata.date_creation)
     }
     return test_ok;
 }
+
+

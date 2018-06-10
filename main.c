27ba4406626078ffea9c8755b34b07f3a91fc217
PC-MAEL-1\maelr_000 <mael.rouxel@outlook.fr>
2018-06-10 14:05:29 +0200

    Inode v1.5
    Définition de macro de test unitaire pour simplifier écriture de tests
    
    promis, prochaine fois j'avance de nouveau

--- main.c
+++ main.c
@@ -10,8 +10,13 @@ bool test_initialisation_INODE();
 bool test_initialisation_DISK();
 
 #define TEST(test_fonc)  printf(test_fonc()? " + %s : OK\n" : " !! %s : KO\n",  #test_fonc );
-#define ERREUR(message, code) printf(message, code); \
-        test_ok = false;
+#define TESTU(test,message,code)\
+              if(test)\
+                {\
+                printf(message, code); \
+                test_ok = false;\
+                }
+
 
 int main()
 {
@@ -29,18 +34,14 @@ bool test_initialisation_DISK()
     bool test_ok = true;
 
     Initialiser_DISK(&p1);
-    if(p1.last_id != 1)
-    {
-        ERREUR("ERR INIT DISK : LAST_ID != 1 (%d)\n", p1.last_id)
-    }
+
+    TESTU(p1.last_id != 1,
+          "ERR INIT DISK : LAST_ID != 1 (%d)\n", p1.last_id)
 
     for(int i=0;i<TAILLE_MAX_DISK;i++)
     {
-        if(p1.superbloc[i]!=NULL)
-        {
-            ERREUR("ERR INIT DISK : SUPERBLOCK NOT NULL (BLOCK %d)\n",i)
-            break;
-        }
+        TESTU(p1.superbloc[i]!=NULL,
+            "ERR INIT DISK : SUPERBLOCK NOT NULL (BLOCK %d)\n",i)
     }
     return test_ok;
 
@@ -51,22 +52,19 @@ bool test_initialisation_INODE()
     bool test_ok = true;
     INODE i;
     Initialiser_INODE(&i);
-    if(i.id != -1)
-    {
-        ERREUR("ERR INIT INODE : ID INITIALIZATION WRONG (id = %d)\n",i.id)
-    }
-    if(i.nom != NULL)
-    {
-        ERREUR("ERR INIT INODE : NAME NOT NULL (%s)\n",i.nom)
-    }
-    if(i.metadata.taille_fichier != 0)
-    {
-        ERREUR("ERR INIT INODE : FILE SIZE NOT NULL (%d)\n",i.metadata.taille_fichier)
-    }
-    if(strcmp(i.metadata.date_creation, "00/00/0000") != 0)
-    {
-        ERREUR("ERR INIT INODE : CREATION DATE INITIALIZATION WRONG (%s)\n",i.metadata.date_creation)
-    }
+
+    TESTU(i.id != -1,
+          "ERR INIT INODE : ID INITIALIZATION WRONG (id = %d)\n",i.id)
+
+    TESTU(i.nom != NULL,
+        "ERR INIT INODE : NAME NOT NULL (%s)\n",i.nom)
+
+    TESTU(i.metadata.taille_fichier != 0,
+        "ERR INIT INODE : FILE SIZE NOT NULL (%d)\n",i.metadata.taille_fichier)
+
+    TESTU(strcmp(i.metadata.date_creation, "00/00/0000") != 0,
+        "ERR INIT INODE : CREATION DATE INITIALIZATION WRONG (%s)\n",i.metadata.date_creation)
+
     return test_ok;
 }
 
